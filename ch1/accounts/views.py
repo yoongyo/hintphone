8c777203e31c851c8f9b8d2293f7b528bf92e204
yoongyo <jyg0172@naver.com>
2019-07-02 16:57:04 +0900

    ad

--- ch1/accounts/views.py
+++ ch1/accounts/views.py
@@ -4,7 +4,21 @@ from django.contrib.auth import login, authenticate
 
 
 def signin(request):
-    if request.method == "POST":
+    if request.COOKIES.get('username') is not None:
+        username = request.COOKIES.get('username')
+        password = request.COOKIES.get('password')
+        method = request.COOKIES.get('method')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            if method == "pc":
+                return HttpResponseRedirect(reverse('hint:admin', args=[user.username]))
+            else:
+                return HttpResponseRedirect(reverse('hint:theme_list', args=[user.username]))
+        else:
+            return HttpResponse('로그인 실패. 다시 시도 해보세요.')
+
+    elif request.method == "POST":
         form = LoginForm(request.POST)
         username = request.POST['username']
         password = request.POST['password']
@@ -13,10 +27,20 @@ def signin(request):
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
+
             if method == "pc":
-                return HttpResponseRedirect(reverse('hint:admin', args=[user.username]))
+                response = HttpResponseRedirect(reverse('hint:admin', args=[user.username]))
+                response.set_cookie('username', username)
+                response.set_cookie('password', password)
+                response.set_cookie('method', method)
+                return response
+
             else:
-                return HttpResponseRedirect(reverse('hint:theme_list', args=[user.username]))
+                response = HttpResponseRedirect(reverse('hint:theme_list', args=[user.username]))
+                response.set_cookie('username', username)
+                response.set_cookie('password', password)
+                response.set_cookie('method', method)
+                return response
         else:
             return HttpResponse('로그인 실패. 다시 시도 해보세요.')
     else:
