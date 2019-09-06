a85b4fc185cb0ab8e42876e7fa4d5e4134a95cf9
yoongyo <jyg0172@naver.com>
2019-09-06 15:22:09 +0900

    Ad

--- ch1/hint/admin.py
+++ ch1/hint/admin.py
@@ -3,7 +3,7 @@ from .models import Theme
 
 
 class ThemeAdmin(admin.ModelAdmin):
-    list_display = ['name', 'roomEscape']
+    list_display = ['name', 'roomEscape', 'enterKey', 'theme_number', 'hintCount']
 
 
 admin.site.register(Theme, ThemeAdmin)
